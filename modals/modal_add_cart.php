1e8a9f1809761ec9bd5338afe3a66b9431a2648f
Henrik Rechtien <henrikRechtien@yahoo.com>
2019-02-19 11:01:32 +0100

    #3 User-Interface + Started with the implementation of the add-cart dialog form

--- modals/modal_add_cart.php
+++ modals/modal_add_cart.php
@@ -13,8 +13,8 @@
 
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="submit" class="btn btn-primary">Save changes</button>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Schliessen</button>
+                <button type="submit" class="btn btn-primary">Wagen anlegen</button>
             </div>
         </form>
         </div>
