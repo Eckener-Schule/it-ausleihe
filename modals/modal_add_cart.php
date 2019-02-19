e3f80f01a287e852253fc46d747549f543e269ad
Henrik Rechtien <henrikRechtien@yahoo.com>
2019-02-19 11:00:35 +0100

    #3 User-Interface + I added the form to add a device in a bootstrap dialog

--- modals/modal_add_cart.php
+++ modals/modal_add_cart.php
@@ -1,6 +1,6 @@
 <div class="modal fade" id="modal_add_cart" tabindex="-1" role="dialog" aria-hidden="true">
     <div class="modal-dialog modal-lg" role="document">
-        <div class="modal-content">
+        <form class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title">Wagen anlegen:</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
@@ -8,12 +8,15 @@
                 </button>
             </div>
             <div class="modal-body">
-                ...
+
+
+
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
+                <button type="submit" class="btn btn-primary">Save changes</button>
             </div>
+        </form>
         </div>
     </div>
 </div>
--- modals/modal_add_device.php
+++ modals/modal_add_device.php
@@ -1,19 +1,32 @@
 <div class="modal fade" id="modal_add_device" tabindex="-1" role="dialog" aria-hidden="true">
     <div class="modal-dialog modal-lg" role="document">
-        <div class="modal-content">
+        <form class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" >Gerät anlegen:</h5>
+                <h5 class="modal-title">Gerät anlegen:</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
-                ...
+                <div class="col-12 row">
+                    <div class="col-sm-12 col-lg-6">
+                        <br>
+                        <p class="form-label">Geräte-ID:</p><input type="text" name="device-id" id="device-id"><br><br>
+                        <p class="form-label">Type:</p><input type="text" name="device-type" id="device-type"><br><br>
+                        <p class="form-label">Hersteller:</p><input type="text" name="device-brand" id="device-brand"><br><br>
+                    </div>
+                    <div class="col-sm-12 col-lg-6">
+                        <br>
+                        <p class="form-label">Name:</p><input type="text" name="device-name" id="device-name"><br><br>
+                        <p class="form-label">Wagen-ID:</p><input type="text" name="device-cart-id" id="device-cart-id"><br><br>
+                        <p class="form-label">Link:</p><input type="text" name="device-link" id="device-link"><br><br>
+                    </div>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Schliessen</button>
+                <button type="submit" class="btn btn-primary">Gerät anlegen</button>
             </div>
-        </div>
+        </form>
     </div>
+</div>
 </div>
