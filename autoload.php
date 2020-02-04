d24d1a45da95e721313ee15e103f3f702cb3eb7f
Marcel Möller <marcelmoeller99@gmx.de>
2020-02-04 09:02:22 +0100

    Add simple autoloader with static paths for classes without namespaces

---
+++ autoload.php
@@ -0,0 +1,20 @@
+<?php
+define('BASEPATH', __DIR__ . DIRECTORY_SEPARATOR);
+
+spl_autoload_register(function ($class) {
+    // Paths to autoload
+    $paths = [
+        "Classes",
+    ];
+
+    // Iterates through all paths and requires all classes of this directory
+    for ($i = 0; $i < count($paths); $i++) {
+        // Contains the full path to the file
+        $filePath = BASEPATH . $paths[$i] . DIRECTORY_SEPARATOR . str_replace('\\', '/', $class) . '.php';
+        // Require class file if it exists
+        if (file_exists($filePath)) {
+            require_once $filePath;
+        }
+    }
+
+});
--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php require_once "./autoload.php"; ?>
 <?php require_once "./header.php"; ?>
 <?php require_once "./navbar.php"; ?>
 
