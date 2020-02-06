6fbd98119f92c0de7fdaf21ec9e14095d6e7015a
LeonVA <vagnoni@gmx.de>
2020-02-06 08:23:48 +0100

    made some comments in the code

--- Classes/ActiveRecord.php
+++ Classes/ActiveRecord.php
@@ -1,9 +1,28 @@
 <?php
+/**
+ * Active Record is a desing pattern 
+ * that allows you to change data in a databse by using an Object.
+**/
 abstract class ActiveRecord
 {
+    /**
+     * it saves the record
+     **/
     abstract public function save();
+    /**
+     * it delets the record
+     **/
     abstract public function delete();
+    /**
+     * it inserts the record
+     **/
     abstract protected function insert();
+    /**
+     * it updates the record
+     **/
     abstract protected function update();
+     /**
+     * @return a specific ID
+     **/
     abstract static function load($id):ActiveRecord;
 }
