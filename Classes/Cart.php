dd1579c55eec72fdc1e87fd4db53b023231dfef7
LeonVA <vagnoni@gmx.de>
2020-02-18 10:12:14 +0100

    made some comments and implemented in every method from the class cart the database connection

--- Classes/Cart.php
+++ Classes/Cart.php
@@ -1,10 +1,13 @@
 <?php
+require('Database.php');
+ 
 /**
  * Description of Cart
  *
  * @author petersen
  * @author lehmann
  * @author jessen
+ * @author Leon Vagnoni
  */
 class Cart {
 
@@ -19,51 +22,56 @@ public function __construct() {
     /**
      * @param $devices
      * @param $name
+     * adds a devices to the database 
      */
 public function addCart($devices, $name) {
-    $db = new mysqli('localhost', 'root', '', 'it-ausleihe');
-    print_r($db->connect_error);
 
-    if($db->connect_errno) {
-        die('Es gibt ein Problem mit der Datenbank');
-    }
-
-
+ 
+    
     for($i=0; $i<$devices;$i++) {
         $insert = "INSERT INTO 'cart' ('cartID', 'deviceID', 'name') VALUES (". $cartID .", ". $devices[$i] .", ". $name .")";
-        $db->query($insert);
+        $params = array( 
+            "devices" => $devices,
+            "name" => $name,
+        );
+        $exporter = Database::getDbConnectiont();
+        $exporter->executeQuery($insert, $params);
     }
+    
 
     }
 
     /**
      * Check the cart about completeness
      * @param $cartID
+     * searchs the typed cartID from the database
      */
 public function checkTotality($cartID) {
-    $db = new mysqli('localhost', 'root', '', 'it-ausleihe');
-    print_r($db->connect_error);
 
-    if($db->connect_errno) {
-        die('Es gibt ein Problem mit der Datenbank');
-    }
+
+
     $select = "SELECT 'cartID', 'deviceID' FROM 'cart' WHERE 'cartID' = ". $cartID;
-    $db->query($select);
+    $params = array( 
+        "cardID" => $cartID,
+    );
+    $exporter = Database::getDbConnectiont();
+    $exporter->executeQuery($select, $params);
 }
 
     /**
      * Deletes cart from database
      * @param $cartID
+     * delete the typed cartID from the database
      */
 public function deleteCart($cartID){
-    $db = new mysqli('localhost', 'root', '', 'it-ausleihe');
-    print_r($db->connect_error);
+  
+    $params = array(
+        "cardID" => $cartID,
+    );
 
-    if($db->connect_errno) {
-        die('Es gibt ein Problem mit der Datenbank');
-    }
     $delete = "DELETE FROM 'cart' WHERE 'cartID' = ". $cartID;
-    $db->query($delete);
+    $exporter = Database::getDbConnectiont();
+    $exporter->executeQuery($delete, $params);
 
 }
 
@@ -71,14 +79,10 @@ public function deleteCart($cartID){
      * Modifies the database entry of the cart
      * @param $device
      * @param $name
+     * modifys the typed mType,device and name from the database
      */
 public function modifyCart($mType, $device, $name){ //Modifies the database entry of the cart
-    $db = new mysqli('localhost', 'root', '', 'it-ausleihe');
-    print_r($db->connect_error);
-
-    if($db->connect_errno) {
-        die('Es gibt ein Problem mit der Datenbank');
-    }
+  
 
     if($mType == "add") {
         $modify = "INSERT INTO 'cart' ('cartID', 'deviceID', 'name') VALUES ( ". $cartID .", ". $devices .", ". $name .")";
@@ -86,7 +90,13 @@ public function modifyCart($mType, $device, $name){ //Modifies the database entr
     else {
         $modify = "DELETE FROM 'cart' WHERE 'devieID' = ". $deviceID;
     }
-    $db->query($modify);
+    $params = array( "mType" => $mType,
+        "device" => $device,
+        "name" => $name,
+    );
+
+    $exporter = Database::getDbConnectiont();
+    $exporter->executeQuery($modify, $params);
 }
 
 }
