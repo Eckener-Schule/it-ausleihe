e933329da1e8ff7cdf72841a5376dc9285392e3a
LeonVA <vagnoni@gmx.de>
2020-02-20 09:12:22 +0100

    added some methods and updated the sql querys

--- Classes/Cart.php
+++ Classes/Cart.php
@@ -1,6 +1,5 @@
 
 <?php
-require('Database.php');
 
 /**
  * Description of Cart
@@ -10,27 +9,96 @@ require('Database.php');
  * @author jessen
  * @author Leon Vagnoni
  */
-class Cart {
+class Cart extends ActiveRecord{
 
- public function __construct() {
-    /**
-     * @param $devices
-     * @param $name
-     * adds a devices to the database 
-     */
+    private $database;
+    private $name;
+    private $cardID;
+
+ public function __construct()
+ {
+    $this->database = Database::getDbConnection();
  }
-public function addCart($devices, $name) {
 
-
-    for($i=0; $i<$devices;$i++) {
-        $insert = "INSERT INTO 'cart' ('cartID', 'deviceID', 'name') VALUES (". $cartID .", ". $devices[$i] .", ". $name .")";
-        $params = array( 
-            "devices" => $devices,
-            "name" => $name,
+ public function save()
+ {   
+     if($this->getCardID() === null) {
+         $this->insert();
+     }
+     else {
+         $this->update();
+     }
+ }
+/**
+     * Updates the Cart in the database
+     * 
+     * {@inheritDoc}
+     * @see ActiveRecord::update()
+     */
+    protected function update()
+    {
+        $query = "UPDATE cart
+            SET name = :name
+            WHERE cardID = :cardID;";
+        $params = array(
+            ':name' => $this->name, 
+            ':cardID' => $this->cardID
         );
-        $exporter = Database::getDbConnectiont();
-        $exporter->executeQuery($insert, $params);
+        
+        $this->database->executeQuery($query, $params);
     }
+
+      /**
+     * Inserts the Cart into the database
+     * 
+     * {@inheritDoc}
+     * @see ActiveRecord::insert()
+     */
+    protected function insert()
+    {
+        $query = "INSERT INTO cart (name) VALUES (:name);";
+        $params = array(
+            ':name' => $this->name, 
+        );
+        
+        $this->database->executeQuery($query, $params);
+    }
+
+     /**
+     * Deletes the Cart in the database
+     * 
+     * {@inheritDoc}
+     * @see ActiveRecord::delete()
+     */
+    public function delete()
+    {
+        $query = "DELETE FROM cart WHERE cardID = :cardID;";
+        $params = array(
+            ':cardID' => $this->cardID
+        );
+        
+        $this->database->executeQuery($query, $params);
+    }
+
+public function addCart($devices, $cartID, $name) {
+
+    $insert = "INSERT INTO 'cart' ('cartID', 'name') VALUES (:cardID, :name)";
+    $paramsInsert = array( 
+        "cartID" => $cardID,
+        "name" => $name
+    );
+    for($i=0; $i<$devices;$i++) {
+        $devices[i];
+
+        $update = "UPDATE 'device' set 'cardID' where 'deviceID' = :".$devices[i].";";
+        $paramsUpdate = array( 
+            "devices" => $devices
+        );
+        $exporter = Database::getDbConnection();
+        $exporter->executeQuery($update, $paramsInsert);
+        
+    }
+    $exporter->executeQuery($insert , $paramsUpdate);
 
 
     }
@@ -42,11 +110,11 @@ public function addCart($devices, $name) {
      */
 public function checkTotality($cartID) {
 
-    $select = "SELECT 'cartID', 'deviceID' FROM 'cart' WHERE 'cartID' = ". $cartID;
+    $select = "SELECT 'cartID' FROM 'cart' WHERE 'cartID' = :".$cardID.";";
     $params = array( 
         "cardID" => $cartID,
     );
-    $exporter = Database::getDbConnectiont();
+    $exporter = Database::getDbConnection();
     $exporter->executeQuery($select, $params);
 }
 
@@ -61,8 +129,8 @@ public function deleteCart($cartID){
         "cardID" => $cartID,
     );
 
-    $delete = "DELETE FROM 'cart' WHERE 'cartID' = ". $cartID;
-    $exporter = Database::getDbConnectiont();
+    $delete = "DELETE FROM 'cart' WHERE 'cartID' = :".$cardID.";";
+    $exporter = Database::getDbConnection();
     $exporter->executeQuery($delete, $params);
 
 }
@@ -75,18 +143,46 @@ public function deleteCart($cartID){
      */
 public function modifyCart($mType, $device, $name){ //Modifies the database entry of the cart
     if($mType == "add") {
-        $modify = "INSERT INTO 'cart' ('cartID', 'deviceID', 'name') VALUES ( ". $cartID .", ". $devices .", ". $name .")";
+        $modify = "INSERT INTO 'cart' ('cartID', 'name') VALUES ( :cartID, :name )".";";
     }
     else {
-        $modify = "DELETE FROM 'cart' WHERE 'devieID' = ". $deviceID;
+        $modify = "DELETE FROM 'device' WHERE 'devieID' = ". $deviceID .";";
     }
     $params = array( "mType" => $mType,
         "device" => $device,
         "name" => $name,
     );
 
-    $exporter = Database::getDbConnectiont();
+    $exporter = Database::getDbConnection();
     $exporter->executeQuery($modify, $params);
 }
 
+    /**
+     * @return int - the ID of the Cart
+     */
+    public function getBorrowerId() 
+    {
+        return $this->cardId;
+    }
+    
+    /**
+     * Sets the firstname of the Cart
+     * 
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+    
+    /**
+     * @return string - the firstname of the Cart
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+    
+    
+    
 }
